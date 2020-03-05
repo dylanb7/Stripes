39f645f2bbc19328d7377805cebf07afc13c039c
Dylan Baker <dylanbaker7@gmail.com>
2020-03-04 19:46:32 -0500

    Initial Commit

---
+++ Stripes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stripes
+//
+//  Created by Dylan Baker on 3/4/20.
+//  Copyright © 2020 Dylan Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
